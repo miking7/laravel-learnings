449bb44adcc9b41c40f0a5b43cc4dc4672d5ac68
Michael Engelbrecht <michaelndani@gmail.com>
2019-12-30 14:41:56 +1100

    TUT: Tests phase 1

--- tests/Feature/SubmitLinksTest.php
+++ tests/Feature/SubmitLinksTest.php
@@ -2,21 +2,43 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SubmitLinksTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    function guest_can_submit_a_new_link()
+    {
+        $response = $this->post('/submit', [
+            'title' => 'Example Title',
+            'url' => 'http://example.com',
+            'description' => 'Example description.',
+        ]);
+
+        $this->assertDatabaseHas('links', [
+            'title' => 'Example Title'
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertHeader('Location', url('/'));
+
+        $this
+            ->get('/')
+            ->assertSee('Example Title');
     }
+
+    /** @test */
+    function link_is_not_created_if_validation_fails()
+    {
+        $response = $this->post('/submit');
+
+        $response->assertSessionHasErrors(['title', 'url', 'description']);
+    }
+    
+
 }
